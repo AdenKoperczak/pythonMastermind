00b9c0e27ddc9777691b8869fcd8f5224c509a42
AdenKoperczak <38887432+AdenKoperczak@users.noreply.github.com>
2019-03-03 16:33:08 -0500

    fixed my bad code

--- Mastermind.py
+++ Mastermind.py
@@ -1,30 +1,36 @@
 import random
 
+numberOfPegs=4
+numberOfGuesses=12
+numberOfColors=6
+
 ch = ""
 
-for i in range(0,4):
-    ch=ch+str(random.randint(1,6))
+for i in range(numberOfPegs):
+    ch=ch+str(random.randint(1,numberOfColors))
 
 i = 0
-while i < 12:
+while i < numberOfGuesses:
     ip = input("Guess: ")
     if ip == "end":
-        i = 12
+        i = numberOfGuesses
     else:
         black = 0
         white = 0
-        for j in range(0,4):
+        for j in range(numberOfPegs):
             if ip[j]==ch[j]:
                black=black+1
             else:
+                con=True
                 for c in ch:
-                    if ip[j]==c:
+                    if ip[j]==c and con:
                         white=white+1
-        if black == 4:
+                        con=False
+        if black == numberOfPegs:
             print("You Win")
-            i = 12
+            i = numberOfGuesses
         else:
             print("black: " + str(black) + " white: " + str(white))
     i = i + 1
-if i == 12:
+if i == numberOfGuesses:
     print("You Lose. The answor was " + ch + ".")
